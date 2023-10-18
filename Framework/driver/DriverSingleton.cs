05d1538c9e7d9e30da5239fa4ef6cca47800b49a
Jurand76 <jurand@interia.pl>
2023-10-18 23:42:16 +0200

    Dodaj pliki projektów.

---
+++ Framework/driver/DriverSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Framework.driver
+{
+    public class DriverSingleton
+    {
+        private static IWebDriver driver;
+
+        private DriverSingleton() { }
+
+        public static IWebDriver getDriver()
+        {
+            if (null == driver)
+            {
+                driver = new ChromeDriver();
+            }
+       
+            return driver;
+        }
+
+        public static void CloseDriver()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver = null;
+            }
+        }
+    }
+}

1bc499865c99ad9ec50cc221be08839a2fcf14f5
Jurand76 <jurand@interia.pl>
2023-10-19 00:09:32 +0200

    feat: usercreator class

---
+++ Framework/service/UserCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework.model;
+
+namespace Framework.service
+{
+    public class UserCreator
+    {
+        private readonly string userName = "tomek";
+        private readonly string userPassword = "tomek1234";
+
+        public User withCredentials()
+        {
+            return new User(userName, this.userPassword);
+        }
+
+        public User withEmptyUserName()
+        {
+            return new User("", userPassword);
+        }
+
+        public User withEmptyUserPassword()
+        {
+            return new User(userName, "");
+        }
+    }
+}
